b049c322a58663af147ff1c2ff2f40d4e9076ac5
abhishek <abhi0108jha@gmail.com>
2023-07-25 13:04:35 +0530

    added frontend.

---
+++ backend/index.js
@@ -0,0 +1,107 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+app.use(express.json())
+app.use(morgan())
+
+morgan('tiny')
+
+let phonebook = [
+    {
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
+    }
+]
+
+const generateId = () => {
+    const maxId = phonebook.length > 0 ?
+        Math.max(...phonebook.map(n => n.id)) : 0
+
+    return maxId + 1;
+}
+
+app.get('/', (request, response) => {
+    response.send('<h1>This is phonebook.</h1>')
+
+})
+
+app.get('/api/persons', (request, response) => {
+    response.send(phonebook)
+})
+
+app.get('/api/info', (request, response) => {
+    const date = new Date();
+    console.log(date)
+    response.send(
+        `<p>Phonebook has info for ${phonebook.length} people</p>
+        <br />
+        <p>${date}</p>`
+    )
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    const contact = phonebook.find(person => person.id == id)
+    if(contact) {
+        response.send(contact)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    const contact = phonebook.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body 
+    if(phonebook.find(pb => pb.name === body.name)) {
+        return response.status(400).json({
+            error: 'name already exist'
+        })
+    }
+    if(!body.name) {
+        return response.status(400).json({
+            error: 'name is missing'
+        })
+    }
+    else if(!body.number) {
+        return response.status(400).json({
+            error: 'number is missing'
+        })
+    }
+    else {
+        const contact = {
+            id: generateId(),
+            name: body.name,
+            number: body.number
+        }
+    
+        phonebook = phonebook.concat(contact)
+        response.json(contact)
+    }
+    
+})
+const PORT= 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
